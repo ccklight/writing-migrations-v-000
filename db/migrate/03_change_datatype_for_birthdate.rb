4e54dddfbe82e6cdc53e0fe672fb413c05dd6a23
Cheryl K <cherylkelson@icloud.com>
2019-01-23 19:59:01 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -2,6 +2,6 @@ class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 
 def change
   change_column(:students, :birthdate, :datetime)
-  #change_column :students do |t|
-    t.datetime :birthday
+
+
   end
